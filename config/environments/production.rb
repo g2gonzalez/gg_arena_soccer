3bb1fcce6c1f51bd954f71454f7d7734df2142e1
Gerardo Gonzalez <gonzalezrgerardo@gmail.com>
2015-03-22 15:36:16 -0700

    Updated CSS file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
 	# Settings specified here will take precedence over those in config/application.rb.
 
 	# Code is not reloaded between requests.
-	config.cache_classes = true
+	config.cache_classes = false
 
 	# Eager load code on boot. This eager loads most of Rails and
 	# your application in memory, allowing both threaded web servers
@@ -20,7 +20,7 @@ Rails.application.configure do
 	# config.action_dispatch.rack_cache = true
 
 	# Disable Rails's static asset server (Apache or nginx will already do this).
-	config.serve_static_assets = true
+	config.serve_static_assets = false
 
 	# Compress JavaScripts and CSS.
 	config.assets.js_compressor = :uglifier
@@ -30,7 +30,7 @@ Rails.application.configure do
 	config.assets.compile = true
 
 	# Generate digests for assets URLs.
-	config.assets.digest = true
+	config.assets.digest = false
 
 	# `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -77,5 +77,5 @@ Rails.application.configure do
 	config.active_record.dump_schema_after_migration = false
 
 	# Required for heroku
-	config.action_mailer.default_url_options = { :host => 'ggarenasoccer.com' }
+	config.action_mailer.default_url_options = { :host => 'ggarenasoccer.herokuapp.com' }
 end
